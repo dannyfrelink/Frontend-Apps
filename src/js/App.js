a820328e5b48e3915cdf1d6e671b42f438b60e23
dannyfrelink <danny-frelink@hotmail.com>
2021-11-29 11:44:20 +0100

    ophalen wereldkaart + hover en begin onClick

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,8 @@
 import '../scss/App.scss';
 import { useEffect, useState } from 'react';
 import getData from './modules/currencyData.js';
-import BarChart from './components/BarChart';
+// import BarChart from './components/BarChart';
+import WorldMap from './components/WorldMap';
 
 function App() {
   const [json, setJson] = useState(null);
@@ -17,7 +18,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Currencies compared to &euro;1</h1>
+        {/* <h1>Currencies compared to &euro;1</h1>
         <p>Filter on continent:</p>
         <form>
           <label><input type='radio' name='filter' value='EU' id='filter' checked={selectedFilter === 'EU'} onChange={onRadioButtonChange} />Europe</label>
@@ -26,11 +27,13 @@ function App() {
           <label><input type='radio' name='filter' value='AF' id='filter' checked={selectedFilter === 'AF'} onChange={onRadioButtonChange} />Africa</label>
           <label><input type='radio' name='filter' value='AS' id='filter' checked={selectedFilter === 'AS'} onChange={onRadioButtonChange} />Asia</label>
           <label><input type='radio' name='filter' value='OC' id='filter' checked={selectedFilter === 'OC'} onChange={onRadioButtonChange} />Oceania</label>
-        </form>
+        </form> */}
         <div className='hidden' id='tooltip'>
           <p className='hidden' id='content'></p>
         </div>
-        {json?.EU && <BarChart data={json[selectedFilter]} />}
+        {/* {json?.EU && <BarChart data={json[selectedFilter]} />} */}
+
+        <WorldMap />
       </header>
     </div>
   );
